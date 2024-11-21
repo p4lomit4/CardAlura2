3cd14548e0ec03292611d10399705977fe0fc5dd
p4lomit4 <00001102294688sp@al.educacao.sp.gov.br>
2024-11-21 20:49:07 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,25 @@
+criaCartao(
+    'Curiosidades Cinema',
+    'Quem inventou o cinema?',
+    'Os irmãos franceses Auguste e Louis Lumière, que, no século XIX, criaram o cinematógrafo '
+)
+
+criaCartao(
+    'Curiosidades Cinema',
+    'O filme de maior bilheteria da história do cinema',
+    'De acordo com um levantamento feito pelo Box Office Mojo, do IMDb, o filme de maior bilheteria da história do cinema até agora é Avatar, de 2009. O longa arrecadou US$ 2,8 bilhões, o que equivale a R$ 14,7 bilhões.. '
+)
+
+criaCartao(
+    'Curiosidades Cinema',
+    'O filme de maior bilheteria da história do cinema nacional',
+    'Segundo um ranking divulgado pelo Instituto de Cinema (InC), o filme de maior bilheteria da história do cinema nacional até hoje é Minha Mãe é uma Peça 3, de 2019. O longa arrecadou R$ 143,9 milhões. '
+)
+
+criaCartao(
+    'Curiosidades Cinema ',
+    'Maior vencedor da história do Oscar',
+    'O maior vencedor da história do Oscar é o produtor cinematográfico norte-americano, Walt Disney, com 26 Oscars e 59 indicações ao prêmio'
+
+
+)
